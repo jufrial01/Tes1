dde03db22c35f4398a6e53620ee6a96ff95870a6
jufrial01 <rennoren171@gmail.com>
2025-07-11 05:55:40 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -183,4 +183,4 @@ window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-});});
+});
